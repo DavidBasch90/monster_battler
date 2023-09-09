d940df0fe890f02f3d6851ca66239b077be57b59
david <davidbasch@live.co.uk>
2023-09-09 13:33:34 +0100

    first push

---
+++ classes.py
@@ -0,0 +1,412 @@
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+# Initialize Pygame Font
+pygame.font.init()
+font = pygame.font.Font(None, 36)
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+FPS = 30
+
+# Load Images
+player_img = pygame.image.load("./assets/images/player.png")
+npc_img = pygame.image.load("./assets/images/npc.png")
+map_img = pygame.image.load("./assets/images/map.png")
+
+
+
+class GameClock:
+    def __init__(self):
+        self.start_time = pygame.time.get_ticks() - 21600000  # Start at 6 AM
+
+    def get_time(self):
+        total_seconds = (pygame.time.get_ticks() - self.start_time) // 1000
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        seconds = total_seconds % 60
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+
+
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, x, y, image, name, health, attack, defense, moves):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.moves = moves
+
+
+
+# Player and NPC classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = player_img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 5
+        self.team = []
+
+    def update(self, keys, house_rects, log_rects, pond_rects):
+        dx = dy = 0
+        if keys[pygame.K_UP]:
+            dy -= self.speed
+        if keys[pygame.K_DOWN]:
+            dy += self.speed
+        if keys[pygame.K_LEFT]:
+            dx -= self.speed
+        if keys[pygame.K_RIGHT]:
+            dx += self.speed
+
+        # Check for potential collisions
+        future_rect = self.rect.move(dx, dy)
+        for house_rect in house_rects:
+            if future_rect.colliderect(house_rect):
+                # Potential collision detected, don't move the player
+                return
+        for log_rect in log_rects:
+            if future_rect.colliderect(log_rect):
+                return
+        for pond_rect in pond_rects:
+            if future_rect.colliderect(pond_rect):
+                return
+
+        # No collisions detected, move the player
+        self.rect = future_rect
+
+
+class NPC(pygame.sprite.Sprite):
+    def __init__(self, x, y, message, defeated_message, team,logger):
+        super().__init__()
+        self.image = npc_img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.message = message
+        self.defeated_message = defeated_message  # Add this line
+        self.dialogue_state = "ask"
+        self.current_message = None
+        self.team = team
+        self.defeated = False
+        self.logger = logger
+        self.key_pressed_after_defeat = False
+
+    def interact(self, player, screen, font):
+        self.logger.info(f'Interact method called. Current dialogue state: {self.dialogue_state}')
+        if self.dialogue_state == "defeated":
+            self.render_text(screen, self.defeated_message)
+            return
+        if self.dialogue_state == "ask":
+            if self.defeated:
+                if self.current_message is None:
+                    self.current_message = self.defeated_message
+                else:
+                    self.current_message = None
+                    self.key_pressed_after_defeat = False
+            else:
+                self.current_message = self.message
+                self.dialogue_state = "wait_for_answer"
+                self.logger.info('npc dialogue: wait for answer')
+        elif self.dialogue_state == "battle":
+            return self.start_battle(player, screen, font)
+            self.logger.info('battle starting')
+        elif self.dialogue_state == "defeated":
+            if self.current_message is None:
+                self.current_message = self.defeated_message
+            else:
+                self.current_message = None
+                self.key_pressed_after_defeat = False
+        self.logger.info(f'Interact method finished. Final dialogue state: {self.dialogue_state}')
+
+    def end_interaction(self):
+        self.interacting = False
+
+    def interactbackup(self,player,screen,font):
+        print(self.dialogue_state)
+        if self.dialogue_state == "ask":
+            if self.defeated:
+                self.current_message = "You're pretty good..."
+                self.dialogue_state = "defeated"
+            else:
+                self.current_message = self.message
+                self.dialogue_state = "wait_for_answer"
+                self.logger.info('npc dialogue: wait for answer')
+        elif self.dialogue_state == "wait_for_answer":
+            self.dialogue_state = "battle"
+            self.logger.info('npc dialogue: battle')
+        elif self.dialogue_state == "battle":
+            return self.start_battle(player, screen, font)
+            self.logger.info('battle starting')
+        elif self.dialogue_state == "defeated":
+            self.dialogue_state = "ask"
+
+
+    def start_battle(self, player,screen,font):
+        return Battle(player.team, self.team, screen, font)
+
+    def render_text(self, screen, text):
+        text_surface = font.render(text, True, (255, 255, 255))
+        screen.blit(text_surface, (50, 50))
+
+import random
+def render_text(screen, font, text, x, y, color=(255, 255, 255)):
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, (x, y))
+
+class Battle:
+    def __init__(self, player_team, npc_team, screen, font):
+        self.player_team = player_team
+        self.npc_team = npc_team
+        self.state = "waiting_for_input"
+        self.current_monster = 0
+        self.menu_state = "main"
+        self.screen = screen
+        self.font = font
+        self.ui = BattleUI(font)
+        self.main_menu = Menu(["Fight", "Items"], 200, 400, font)
+        self.moves_menu = None
+        self.player_selected_move = False
+        self.message = ""
+        self.message_queue = []
+        self.win_message_displayed = False
+        self.lose_message_displayed = False
+
+
+    def is_npc_defeated(self):
+        return len(self.npc_team) == 0
+
+    def draw(self, screen):
+        battlebg = pygame.image.load("./assets/images/battlebg.jpg")
+        screen.fill((0, 0, 0))  # Fill the screen with black color
+        screen.blit(battlebg, (0, 0))
+
+        if self.player_team:
+            player_monster = self.player_team[0]
+            screen.blit(player_monster.image, (100, 300))  # Draw player's active monster
+            self.ui.draw_health_bar(screen, 100, 330, player_monster.health / 100)  # Draw player monster health bar
+
+        if self.npc_team:
+            npc_monster = self.npc_team[0]
+            screen.blit(npc_monster.image, (500, 100))  # Draw NPC's active monster
+            self.ui.draw_health_bar(screen, 500, 130, npc_monster.health / 100)  # Draw NPC monster health bar
+
+        # Draw battle menu
+        if self.state == "waiting_for_input":
+
+            if self.menu_state == "main":
+                self.main_menu.draw(screen)
+            elif self.menu_state == "moves":
+                if self.moves_menu is None:
+                    moves = [move.name for move in self.player_team[0].moves]
+                    self.moves_menu = Menu(moves, 200, 400, self.font)
+                self.moves_menu.draw(screen)
+
+            # Draw message bar
+            message_x = 50
+            message_y = 500
+            message_width = WIDTH - 2 * message_x
+            message_height = 50
+            pygame.draw.rect(screen, (0, 0, 0),
+                             (message_x, message_y, message_width, message_height))  # Black background
+            if self.message_queue:
+
+                render_text(screen, self.font, self.message_queue[0], message_x + 10, message_y + 10,
+                            (255, 255, 255))  # White text
+
+            # Update the display
+
+
+            pygame.display.flip()
+
+    def use_move(self, attacker, move_name, defender):
+        for move in attacker.moves:
+            if move.name == move_name:
+                damage = int(attacker.attack * move.power - defender.defense)
+                defender.health = max(defender.health - damage, 0)
+                message = f"{attacker.name} used {move.name}!"
+                if random.random() >= move.accuracy:
+                    message += " The move missed!"
+                else:
+                    message += f" It dealt {damage} damage!"
+                self.message_queue.append(message)
+                break
+
+    class Move:
+        def __init__(self, name, power, accuracy):
+            self.name = name
+            self.power = power
+            self.accuracy = accuracy
+
+        def use(self, attacker, defender):
+            if random.random() < self.accuracy:
+                damage = int(attacker.attack * self.power - defender.defense)
+                defender.health = max(defender.health - damage, 0)
+            else:
+                print(f"{attacker.name}'s attack missed!")
+
+    def handle_input(self, events):
+        # Iterate through the list of events
+        for event in events:
+            # Check if the event is a key press
+            if event.type == pygame.KEYDOWN:
+                # Check if the UP arrow key was pressed
+                if event.key == pygame.K_UP:
+                    # Navigate up in the current menu
+                    menu = self.main_menu if self.menu_state == "main" else self.moves_menu
+                    menu.navigate(-1)
+                # Check if the DOWN arrow key was pressed
+                elif event.key == pygame.K_DOWN:
+                    # Navigate down in the current menu
+                    menu = self.main_menu if self.menu_state == "main" else self.moves_menu
+                    menu.navigate(1)
+
+                # Check if the 'Z' key was pressed
+                elif event.key == pygame.K_z:
+                    if self.message_queue:
+                        # If the current message is "Enemy monster fainted!", remove the defeated monster
+                        if self.message_queue[0] == "Enemy monster fainted!":
+                            self.npc_team.pop(0)  # Remove defeated monster
+                        self.message_queue.pop(0)
+                    # If the menu state is 'main'
+                    if self.menu_state == "main":
+                        selected_item = self.main_menu.get_selected_item()
+                        # If the selected item is 'Fight', switch to the moves menu
+                        if selected_item == "Fight":
+                            self.menu_state = "moves"
+                            moves = [move.name for move in self.player_team[0].moves]
+                            self.moves_menu = Menu(moves, 200, 400, self.font)
+                        # If the selected item is 'Items', handle item usage (not implemented yet)
+                        elif selected_item == "Items":
+                            pass
+                    # If the menu state is 'moves'
+
+
+
+                # Check if the 'X' key was pressed
+                elif event.key == pygame.K_x:
+                    # If the menu state is 'moves', go back to the main menu
+                    if self.menu_state == "moves":
+                        self.menu_state = "main"
+
+    def update(self, events):
+        battle_done = False
+
+
+        if self.message_queue:
+            return None  # Don't proceed with other logic until messages are cleared
+
+
+        if self.state == "npc_turn":
+            if not self.npc_team:
+                return
+            if not self.player_team:
+                return
+            current_monster = self.npc_team[self.current_monster]
+            move_name = random.choice(self.npc_team[0].moves).name
+            self.use_move(current_monster, move_name, self.player_team[0])
+            print('npc picking turn')
+            if self.player_team[0].health == 0:
+                self.player_team.pop(0)
+                self.message_queue.append("Player's monster fainted!")
+                self.state = "waiting_for_input"
+
+
+            else:  # Add this else statement
+                self.state = "waiting_for_input"
+
+
+
+
+        elif self.state == "waiting_for_input":
+
+            if not self.message_queue:
+
+                for event in events:
+
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_z:
+
+                        # Check if the menu state is 'moves'
+
+                        if self.menu_state == "moves":
+
+                            if not self.player_selected_move:
+
+                                self.player_selected_move = True
+
+                            else:
+
+                                if not self.npc_team:
+                                    return
+
+                                current_monster = self.player_team[self.current_monster]
+
+                                selected_move = self.moves_menu.get_selected_item()
+
+                                self.use_move(current_monster, selected_move, self.npc_team[0])
+
+                                if self.npc_team[0].health == 0:  # Check if NPC's monster is dead
+                                    self.message_queue.append("Enemy monster fainted!")
+                                    self.state = "waiting_for_input"  # Change state to waiting_for_input
+                                else:
+                                    self.state = "npc_turn"
+
+
+
+        #if the player's team or the npc team is defeated
+        if not self.player_team or not self.npc_team:
+            if not self.npc_team and not self.win_message_displayed:
+                self.message_queue.append("You win!")
+
+                self.win_message_displayed = True
+                return True
+            elif not self.player_team and not self.lose_message_displayed:
+                self.message_queue.append("You lose!")
+                self.lose_message_displayed = True
+                return True
+
+        return None
+
+
+class Menu:
+    def __init__(self, items, x, y, font):
+        self.items = items
+        self.x = x
+        self.y = y
+        self.font = font
+        self.selected_item = 0
+
+    def draw(self, screen):
+        for i, item in enumerate(self.items):
+            color = (255, 255, 255) if i != self.selected_item else (255, 255, 0)
+            render_text(screen, self.font, item, self.x, self.y + i * 50, color)
+
+    def navigate(self, direction):
+        self.selected_item = (self.selected_item + direction) % len(self.items)
+
+    def get_selected_item(self):
+        return self.items[self.selected_item]
+
+class BattleUI:
+    def __init__(self, font):
+        self.font = font
+
+    def draw_health_bar(self, screen, x, y, health_percentage):
+        pygame.draw.rect(screen, (255, 0, 0), (x, y, 100, 10))  # Red background
+        pygame.draw.rect(screen, (0, 255, 0), (x, y, 100 * health_percentage, 10))  # Green health bar
+
+    def draw_main_menu(self, screen, menu):
+        menu.draw(screen)
+
+    def draw_moves_menu(self, screen, menu):
+        menu.draw(screen)
---
+++ main.py
@@ -0,0 +1,241 @@
+import pygame
+import sys
+import logging
+from classes import *
+from functions import *
+
+# Configure logging settings
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler(),
+                              logging.FileHandler('./output/game.log', mode='w')])
+
+TILESIZE = 16
+# Initialize Pygame
+pygame.init()
+# Initialize Pygame Font
+pygame.font.init()
+font = pygame.font.Font(None, 36)
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+SCALE_FACTOR = 2
+FPS = 30
+WHITE = (255, 255, 255)
+
+# Load Images
+player_img = pygame.image.load("./assets/images/player.png")
+player_img = pygame.transform.scale(player_img, (player_img.get_width() * SCALE_FACTOR, player_img.get_height() * SCALE_FACTOR))
+npc_img = pygame.image.load("./assets/images/npc.png")
+npc_img = pygame.transform.scale(npc_img, (npc_img.get_width() * SCALE_FACTOR, npc_img.get_height() * SCALE_FACTOR))
+map_img = pygame.image.load("./assets/images/map.png")
+battlebg = pygame.image.load("./assets/images/battlebg.jpg")
+
+
+#moves
+move1 = Battle.Move("Tackle", 10, 0.95)
+move2 = Battle.Move("Scratch", 8, 0.9)
+move3 = Battle.Move("Tail Whip", 5, 1.0)
+
+# Create monster instances
+player_monster1 = Monster(200, 100, pygame.image.load("./assets/images/player_monster1.png"), "Player Monster 1", 200, 20, 10, [move1, move2, move3])
+player_monster2 = Monster(200, 100, pygame.image.load("./assets/images/player_monster2.png"), "Player Monster 2", 200, 20, 10,[move1, move2, move3])
+npc_monster1 = Monster(100, 100, pygame.image.load("./assets/images/npc_monster1.png"), "NPC Monster 1", 200, 20, 10,[move1, move2, move3])
+npc_monster2 = Monster(100, 100, pygame.image.load("./assets/images/npc_monster2.png"), "NPC Monster 2", 200, 20, 10,[move1, move2, move3])
+
+#Create player, player team, npc, npc team
+player = Player(400, 400)
+player.team = [player_monster1, player_monster2]  # Add monsters to the player's team
+npcs = pygame.sprite.Group()
+npcs.add(NPC(200, 450, "Hello! How are you?","You're decent...", [npc_monster1], logging))
+npcs.add(NPC(600, 400, "Welcome to our town!", "You suck!", [npc_monster2], logging))
+
+# Set up the display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Map")
+
+# Set up the clock
+clock = pygame.time.Clock()
+game_clock = GameClock()
+
+#set flags
+running = True
+battle_active = False
+interacting_npc = None
+battle_npc = None
+game_over = False
+
+# Load the sprite sheets
+sprite_sheet = pygame.image.load('./assets/images/GRASS+.png')
+village_sheet = pygame.image.load('./assets/images/SERENE_VILLAGE_REVAMPED/Serene_Village_16x16.png')
+
+# Define the rectangle that corresponds to the location of the  sprite on the sprite sheet
+grass_rect = pygame.Rect(167, 95, 16, 16)
+stump_rect = pygame.Rect(160, 192, 16, 16)
+flower_rect = pygame.Rect(96,192, 16,16)
+house_rect = pygame.Rect(95,335,60,60)
+pond_rect = pygame.Rect(176,0, 47,50)
+
+# Extract the  sprites
+grass_image = village_sheet.subsurface(grass_rect)
+stump_image = sprite_sheet.subsurface(stump_rect)
+flower_image = sprite_sheet.subsurface(flower_rect)
+house_image = village_sheet.subsurface(house_rect)
+pond_image = village_sheet.subsurface(pond_rect)
+
+#scale the sprites
+stump_image = scale_sprite(stump_image, SCALE_FACTOR)
+flower_image = scale_sprite(flower_image, SCALE_FACTOR)
+house_image = scale_sprite(house_image, SCALE_FACTOR)
+pond_image = scale_sprite(pond_image, SCALE_FACTOR)
+
+#list of coords for where to place the sprites
+log_positions = [(90, 89), (200, 300), (500, 400)]
+flower_positions = [(0, 100), (200, 100), (300, 100), (200, 200), (300, 200), (400, 200), (0, 300)]
+house_positions = [(0, 100), (200, 100), (100, 200), (300, 200)]
+
+##get list of all sprites for collison
+house_rects = [pygame.Rect(x, y, house_image.get_width(), house_image.get_height()) for x, y in house_positions]
+log_rects = [pygame.Rect(x,y,stump_image.get_width(), stump_image.get_height()) for x, y in log_positions]
+pond_rects = [pygame.Rect(200,330,pond_image.get_width(),pond_image.get_height())]
+
+## GAME LOOP
+
+while running:
+    #time
+    clock.tick(FPS)
+    current_time = game_clock.get_time()
+    #button presses
+    keys = pygame.key.get_pressed()
+    # Handle events
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            logging.info('QUIT GAME')
+            running = False
+        if event.type == pygame.KEYDOWN:
+            logging.info('keydown')
+            if game_over:
+                running = False
+            if interacting_npc:
+                print(interacting_npc.dialogue_state)
+            if interacting_npc and interacting_npc.dialogue_state == "defeated":
+                interacting_npc.key_pressed_after_defeat = True
+                interacting_npc = None
+            if event.key == pygame.K_z and not battle_active:
+                logging.info('non battle key press: z')
+                print(interacting_npc)
+                if interacting_npc is None:
+                    collided_npcs = pygame.sprite.spritecollide(player, npcs, False)
+                    print(collided_npcs)
+                    for npc in collided_npcs:
+                        interacting_npc = npc
+                        logging.info('npc interacting with player')
+                        print('a npc is interacting')
+                        interacting_npc.interact(player, screen, font)
+            if interacting_npc:
+                if interacting_npc:
+                    if event.key == pygame.K_y:
+                        logging.info('player says yes to battle')
+                        if interacting_npc.dialogue_state == "wait_for_answer":
+                            interacting_npc.dialogue_state = "battle"
+                            battle = interacting_npc.interact(player, screen, font)
+                            if battle:
+                                battle_active = True
+                                battle_npc = interacting_npc
+                    if event.key == pygame.K_n:
+                        logging.info('player says no to battle')
+                        if interacting_npc.dialogue_state == "wait_for_answer":
+                            interacting_npc.dialogue_state = "ask"
+                            interacting_npc = None  # Add this line
+
+    # Update game objects
+    if not battle_active:
+        player.update(keys, house_rects,log_rects,pond_rects )
+
+    if game_over:
+        screen.fill((255, 255, 255))  # Fill the screen with white color
+        game_over_text = font.render("GAME OVER", True, (0, 0, 0))  # Create a "GAME OVER" text
+        screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2 - game_over_text.get_height() // 2))  # Draw the "GAME OVER" text in the center of the screen
+        pygame.display.flip()  # Update the display to show the "GAME OVER" message
+
+        continue  # Skip the rest of the loop
+
+
+    if battle_active:
+        battle.handle_input(events)
+        battle_done = battle.update(events)
+
+        if battle_done is True:
+
+
+            print('what about here?')
+            logging.info('ending battle')
+            battle_active = False
+            if not battle.npc_team:  # Check if the NPC team is empty
+                logging.info('empty team for npc')
+                if battle_npc is not None:  # Check if battle_npc is not None
+                    logging.info('npc defeated check')
+                    battle_npc.defeated = True
+                    battle_npc.dialogue_state = "defeated"
+                battle_npc = None  # Reset battle_npc to None
+
+                if interacting_npc is not None:  # Add this check
+                    logging.info('npc defeated check')
+                    interacting_npc.defeated = True
+                    interacting_npc.dialogue_state = "defeated"
+                    interacting_npc.end_interaction()
+                    interacting_npc = None
+            else:
+
+
+                game_over = True  # Set the game state to 'game over'
+
+
+    # Draw everything
+    if not battle_active:
+        screen.blit(map_img, (0, 0))
+        # Assuming TILE_SIZE is the size of your grass sprite
+        for y in range(0, HEIGHT, TILESIZE):
+            for x in range(0, WIDTH, TILESIZE):
+                screen.blit(grass_image, (x, y))
+
+
+        # Blit sprites
+        for pos in log_positions:
+            screen.blit(stump_image, pos)
+        for pos in flower_positions:
+            screen.blit(flower_image, pos)
+        for pos in house_positions:
+            screen.blit(house_image, pos)
+        screen.blit(pond_image, (200,330))
+
+        screen.blit(player.image, player.rect)
+        npcs.draw(screen)
+
+
+
+
+        if interacting_npc:
+            state_text = font.render(f"Dialogue State: {interacting_npc.dialogue_state}", True, WHITE)
+
+            if interacting_npc.dialogue_state == "wait_for_answer":
+                interacting_npc.render_text(screen,
+                                            interacting_npc.current_message + " Do you want to battle? (Y/N)")
+            elif interacting_npc.dialogue_state == "ask" or interacting_npc.dialogue_state == "defeated":
+                interacting_npc.render_text(screen, interacting_npc.defeated_message)
+
+
+    else:
+        battle.draw(screen)
+
+    # Update the display
+    time_text = font.render(str(game_clock.get_time()), True, (255, 255, 255))  # Render the time as white text
+    screen.blit(time_text, (WIDTH - time_text.get_width(), 0))  # Draw the text at the top right of the screen
+
+    pygame.display.flip()
+
+# Quit the game
+pygame.quit()
+sys.exit()
+
